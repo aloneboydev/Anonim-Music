f60937d4b19c4079252c6825397a364abe49a0ad
BotName <90719292+UserbotMaps@users.noreply.github.com>
2021-12-01 20:07:47 +0700

    Update song.py

--- Music/Plugins/song.py
+++ Music/Plugins/song.py
@@ -176,7 +176,7 @@ async def mpthree(_, message: Message):
             ID5 = (result[4]["id"])
         except Exception as e:
             return await mystic.edit_text(f"❌ Soung Not Found.\n**Possible Reason:**{e}")
-        thumb ="cache/20211103_135204.jpg"
+        thumb ="cache/1638363845612.jpg"
         await mystic.delete()   
         buttons = search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, duration4, duration5, user_id, query)
         hmo = await message.reply_photo(
