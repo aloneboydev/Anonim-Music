fd3c50e0facb3a47a4dc07ae06a5e3f1053eeb4d
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-23 22:26:23 +0700

    Update start.py

--- Music/Plugins/start.py
+++ Music/Plugins/start.py
@@ -47,7 +47,7 @@ pstart_markup = InlineKeyboardMarkup(
         ],
         [
             InlineKeyboardButton(
-                "ᴏᴡɴᴇʀ", url=f"https://t.me/Kayoshineey"),
+                "ᴏᴡɴᴇʀ", url=f"https://t.me/Bisubiarenak"),
             InlineKeyboardButton("ᴄᴏᴍᴍᴀɴᴅ", url="https://telegra.ph/Hiroshi-12-28-2"),
         ],
     ]
